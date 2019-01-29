f20183d00e2385bfecf33c674621435139bab318
Estevan Cruz <Estevancruz@Estevans-MacBook-Pro.local>
2019-01-29 16:23:42 -0600

    User inputs on js are console.logging

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,11 +11,39 @@
 
 
     // on click function that sends data to firebase in a table called table-data from my ID
+
+    var currentTime = Date.now();
+    var randomFormat = "MM/DD/YYYY";
+
     $("#submit").on("click", function() {
+        event.preventDefault();
         // current time 
         var currentTime = Date.now();
+        //adds to firebase database 
+        var ref = firebase.database().ref("table-data").child(
+            currentTime).set({
+            [currentTime]: true
+            });
+        // format the unix time to hh:mm in console.log
         console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-        });
-    
+        
+        // getting the user input into the console that assigns a value to the id's in html
+        var trainName = $("#trainName").val() || '';;
+        var destination = $("#destination").val() || '';;
+        var firstTrainTime = $("#firstTrainTime").val() || '';;
+        var frequency = $("#frequency").val() || '';;
+        console.log("TRAIN NAME: " + trainName);
+        console.log("TRAIN DESTINATION: " + destination);
+        console.log("FIRST TRAIN TIME: " + firstTrainTime);
+        console.log("EVERY: " + frequency + " MINUTES");
+
+        // new train variable for user inputs
+        var newTrain = {
+            train: trainName,
+            destination: destination,
+            initialTime: firstTrainTime,
+            frequency: frequency,
+        }});
+
     
 
